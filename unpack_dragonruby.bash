62a1451f79616064c70ce883f604bbfa2a50366a
Tanguy Andreani <tanguy.andreani@tuta.io>
2020-08-18 13:35:51 +0200

    fix script for unpacking dragonruby in the current directory

--- unpack_dragonruby.bash
+++ unpack_dragonruby.bash
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
+set -e
 
-# sudo mkdir /opt/user
-# chown user:user /opt/user
-# ./unpack_dragonruby.bash ~/Downloads/dragonruby-gtk-linux-amd64.zip /opt/user/
+# ./unpack_dragonruby.bash ~/Downloads/dragonruby-gtk-linux-amd64.zip .
 
 ZIP=${1}
 INSTALLDIR=${2}
 
-unzip ${ZIP} -d ${INSTALLDIR}
+DRDIR="dragonruby-linux-amd64"
+
+unzip ${ZIP}
+
+rm -rv ${DRDIR}/{mygame,samples}
+
+mv ${DRDIR}/* ${INSTALLDIR}/
+mv ${DRDIR}/.dragonruby ${INSTALLDIR}/
+
+rm -rf ${DRDIR}
