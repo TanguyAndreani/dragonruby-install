507b37e4ab903f3becce22aaee4789233bdbffa6
Tanguy Andreani <tanguy.andreani@tuta.io>
2020-08-18 20:14:57 +0200

    add an usage to the script

--- unpack_dragonruby.bash
+++ unpack_dragonruby.bash
@@ -3,6 +3,20 @@ set -e
 
 # ./unpack_dragonruby.bash ~/Downloads/dragonruby-gtk-linux-amd64.zip .
 
+if [ "$#" -ne 2 ]; then
+    echo "USAGE"
+    echo "	${0} dragonruby.zip target"
+    echo
+    echo "	dragonruby.zip	DragonRuby from itch.io"
+    echo "	target		directory where your mygame folder is"
+    echo
+    echo "EXAMPLE"
+    echo "	\$ ls ."
+    echo "	mygame"
+    echo "	\$ ${0} ~/Downloads/dragonruby-gtk-linux-amd64.zip ."
+    exit 1
+fi
+
 ZIP=${1}
 INSTALLDIR=${2}
 
