40309844b89b5d7fa286bc0a2dfa008e24cb150e
Tanguy Andreani <tanguy.andreani@tuta.io>
2020-08-18 20:23:00 +0200

    remove a useless mv

--- unpack_dragonruby.bash
+++ unpack_dragonruby.bash
@@ -26,7 +26,6 @@ unzip ${ZIP}
 
 rm -rv ${DRDIR}/{mygame,samples}
 
-mv ${DRDIR}/* ${INSTALLDIR}/
-mv ${DRDIR}/.dragonruby ${INSTALLDIR}/
+mv ${DRDIR}/* ${DRDIR}/.dragonruby ${INSTALLDIR}/
 
 rm -rf ${DRDIR}
